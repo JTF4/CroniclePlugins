7df6a5d6561b61b95562added4527a21d080fe1d
David Stevens <david@revisionoptix.com>
2020-09-09 23:38:38 -0400

    Create Visio-Api-Connector.py

---
+++ Visio-Api-Connector.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import socket
+import time
+import os
+
+try:
+    stdinput = sys.stdin.readline()
+    data = json.loads(stdinput)
+
+    ip = data['params']['ip']
+    command = data['params']['command']
+    token = data['params']['token']
+    pin = data['params']['pin']
+    port = data['params']['port']
+
+    if (command == 'on'):
+        stream = os.popen('pyvizio --ip=' + ip + ':' + port + ' --auth=' + token + ' power on')
+        output = stream.read()
+        output
+
+    print('{ "complete": 1 }')
+except:
+    print('{ "complete": 1, "code": 999, "description": "Failed to execute." }')
